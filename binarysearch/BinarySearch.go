4e2baa06963bf1cc037eba3df3df7b06081226a6
Ridz Chandra <ridzchandra9@gmail.com>
2020-09-30 05:29:42 +1000

    corrections

--- binarysearch/BinarySearch.go
+++ binarysearch/BinarySearch.go
@@ -1,4 +1,4 @@
-package interfaces
+package binarysearch
 
 // BinarySearch ...An interface to perform binary search iteratively
 type BinarySearch interface {
--- binarysearch/stringList.go
+++ binarysearch/stringList.go
@@ -1,4 +1,4 @@
-package structs
+package binarysearch
 
 type stringList []string
 
